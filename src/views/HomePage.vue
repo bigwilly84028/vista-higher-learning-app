d3241f6f3b633c7b28247280f38c60cf02e61a40
William Watson <wwatson@Williams-Laptop.local>
2024-12-10 11:32:22 -0500

    adding buttons with icons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,21 +14,93 @@
       </ion-header>
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>Say the vocabulary words.</strong>
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              <ion-button shape="round" color="warning">
+                <ion-icon slot="icon-only" name="square"></ion-icon>
+              </ion-button>
+            </ion-col>
+            <ion-col>
+              <ion-button shape="round" color="danger">
+                <ion-icon slot="icon-only" name="ellipse"></ion-icon>
+              </ion-button>
+            </ion-col>
+            <ion-col>
+              <ion-button shape="round" color="primary">
+                <ion-icon slot="icon-only" name="caret-forward"></ion-icon>
+              </ion-button>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+        <!-- <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p> -->
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonIcon,
+} from "@ionic/vue";
+import { square, ellipse, caretForward } from "ionicons/icons";
+import { addIcons } from "ionicons";
+
+export default {
+  name: "HomePage",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButton,
+    IonIcon,
+  },
+  created: function () {
+    addIcons({
+      square: square,
+      ellipse: ellipse,
+      "caret-forward": caretForward,
+    });
+  },
+  setup() {
+    //const state = "athing";
+
+    return {
+      square,
+      ellipse,
+      caretForward,
+    };
+  },
+};
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +116,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
